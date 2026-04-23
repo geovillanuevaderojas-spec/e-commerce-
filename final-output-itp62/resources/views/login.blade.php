697ef992b40c74934d6f6d289d770565acc46883
carlcain <carljustinecain@gmail.com>
2026-04-24 01:30:18 +0800

    login

---
+++ final-output-itp62/resources/views/login.blade.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Instrument+Sans' rel='stylesheet'>
+</head>
+
+<body>
+
+    <h1 class="login-title">kanlungan.</h1>
+
+    <button class="login_button" id="login_button" type="button">Log In</button>
+    <button class="signin_button" id="signin_button" type="button">Sign In</button>
+
+    <img class="login-img" src="{{asset('images/Gemini_Generated_Image_3hf23p3hf23p3hf2.png')}}" alt="login-img">
+
+    <div class="login" id="login">
+        <form method="POST">
+            @csrf
+            <label for="email">Email</label> <br>
+            <input type="email" placeholder="Enter Email" name="email" id="email"> <br>
+            <p id="emailError" class="validation"></p>
+
+            <label for="password">Password</label> <br>
+            <input type="password" placeholder="Enter Password" name="password" id="password"> <br>
+            <p id="password_Error" class="validation"></p>
+
+            <input class="loginbutton" id="loginbutton" type="submit" value="Log In">
+        </form>
+    </div>
+
+    <div class="sign-in" id="sign-in">
+        <form method="POST">
+            @csrf
+            <label for="name">Name</label> <br>
+            <input type="text" placeholder="Enter Name" name="name" class="name_detail" id="signin_nameDetail"> <br>
+            <p id="nameError" class="validation"></p>
+            
+            <label for="email">Email</label> <br>
+            <input type="email" placeholder="Enter Email" name="email" id="signin_email"> <br>
+            <p id="signin-emailError" class="validation"></p>
+
+            <label for="password">Password</label> <br>
+            <input type="password" placeholder="Enter Password" name="password" id="signin_password"> <br>
+            <p id="signin-passwordError" class="validation"></p>
+
+            <input class="loginbutton" id="signinButton" type="submit" value="Sign In">
+        </form>
+    </div>
+
+    <script src="{{asset('js/login.js')}}"></script>
+</body>
+</html>
