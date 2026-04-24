bf6975c82f4804651595c716beaa5241fd3570f5
Mark-Jooswa <markjoshua.camama@gmail.com>
2026-04-24 21:55:23 +0800

    Views and Routes (#1)
    * login
    
    * homepage
    
    * Made views and routes
    
    * Added vista lounge views and images
    
    ---------
    
    Co-authored-by: carlcain <carljustinecain@gmail.com>

--- final-output-itp62/routes/web.php
+++ final-output-itp62/routes/web.php
@@ -3,5 +3,41 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/home', function () {
+    return view('homepage');
+});
+
+Route::get('/boho_nook', function () {
+    return view('boho_nook');
+});
+
+Route::get('boho_nook_info', function () {
+    return view('boho_nook_info');
+});
+
+Route::get('booking', function () {
+    return view('booking');
+});
+
+Route::get('downtown_oasis', function () {
+    return view('downtown_oasis');
+});
+
+Route::get('downtown_oasis_info', function () {
+    return view('downtown_oasis_info');
+});
+
+Route::get('unit', function () {
+    return view('unit');
+});
+
+Route::get('vista_lounge', function () {
+    return view('vista_lounge');
+});
+
+Route::get('vista_lounge_info', function () {
+    return view('vista_lounge_info');
 });
