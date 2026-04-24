460bc3ca014c14cacf3c2e0e11e5f50d2276b4d7
carlcain <carljustinecain@gmail.com>
2026-04-24 19:33:29 +0800

    modified homepage view

--- final-output-itp62/resources/views/homepage.blade.php
+++ final-output-itp62/resources/views/homepage.blade.php
@@ -14,7 +14,37 @@
         <p class="homepage_paragraphs">Choose from our exclusive selection of 3 units</p>
 
        <div class="three_units" id="units-container">
+           @foreach ($row as $unit)
+         <a href="{{$unit['page']}}" class="unit_one_link">
+          <div class="unit_one">
 
+              <video loop muted poster="{{$unit['mainpic']}}">
+                  <source src="{{$unit['video']}}" type="video/mp4">
+              </video>
+
+              <p class="unitone_details">{{$unit['details']}}</p>
+              <p class="name_of_unit">{{$unit['nameOfUnit']}}</p>
+              <p class="unitone_description">{{$unit['unitDescription']}}</p>
+
+              <div class="unitone_location">
+                  <img class="location_icon" src="{{$unit['location']}}">
+                  <p class="location_name">{{$unit['location-name']}}</p>
+              </div>
+
+              <div class="unit_details">
+                  <div class="guests">
+                      <img src="{{asset('images/cake.png')}}" class="location_icon">
+                      <p class="location_name">{{$unit['guest']}}</p>
+                  </div>
+
+                  <div class="beds">
+                      <img src="{{asset('images/single-bed.png')}}" class="location_icon">
+                      <p class="location_name">{{$unit['beds']}}</p>
+                  </div>
+              </div>
+          </div>
+        </a>
+        @endforeach
         </div>
 
     <footer class="site-footer">
